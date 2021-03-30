51f81250ef721995660e7725a302737e44c4d185
Chrysaoros <s.stojanovic963@gmail.com>
2021-03-30 22:17:48 +0200

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="section">
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  onFormSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return <div>SearchBar</div>;
+  }
+}
+
+export default SearchBar;
